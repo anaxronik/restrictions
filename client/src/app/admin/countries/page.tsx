d3e99cb7c03d39e5c0d028a74f1544e66365bf5e
Sergei Komarov <komarov.s@cdek.ru>
2024-05-11 20:54:49 +0500

    wip

--- client/src/app/admin/countries/page.tsx
+++ client/src/app/admin/countries/page.tsx
@@ -1,30 +1,82 @@
 "use client";
 import APIS from "@/api";
-import { Button } from "primereact/button";
-import { useEffect, useState } from "react";
+import { CountryEntity } from "@/api/api/api";
+import { InputText } from "primereact/inputtext";
+import { Toast } from "primereact/toast";
+import { useEffect, useRef, useState } from "react";
 
 export default function CountriesPage() {
-  const [value, setValue] = useState(0);
+  const [countries, setCountries] = useState<CountryEntity[]>([]);
+  const [name, setName] = useState("");
+  const toast = useRef<Toast>(null);
+
+  const updateCountries = () => {
+    return APIS.api
+      .countriesControllerFindAll()
+      .then((res) => {
+        setCountries(res.data);
+      })
+      .catch(() => {
+        setCountries([]);
+      });
+  };
 
   useEffect(() => {
-    APIS.api.countriesControllerFindAll().then((res) => {
-      console.log({ res });
-    });
-
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    updateCountries();
   }, []);
 
   return (
     <div className={""}>
-      <div>{value}</div>
-      <Button
-        label="add"
-        onClick={() => {
-          setValue((prev) => prev + 1);
-        }}
-      />
+      <Toast ref={toast} />
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("submit");
+            APIS.api
+              .countriesControllerCreate({ name })
+              .then(() => {
+                setName("");
+              })
+              .catch((err: any) => {
+                console.log({ err });
+
+                toast.current?.show({
+                  severity: "error",
+                  summary: err?.response?.data?.message,
+                  detail: err?.response?.data?.message,
+                  life: 3000,
+                });
+              })
+              .finally(() => {
+                updateCountries();
+              });
+          }}
+        >
+          <InputText value={name} onChange={(e) => setName(e.target.value)} />
+        </form>
+      </div>
+      <div className="flex flex-column">
+        {countries.map((c) => {
+          return (
+            <div key={c.id}>
+              <span>{c.name}</span>
+              <span>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    APIS.api.countriesControllerRemove(c.id).then(() => {
+                      updateCountries();
+                    });
+                  }}
+                >
+                  delete
+                </button>
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
